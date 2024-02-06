0f53aac83a83848a7c411d12a6bf910f3ba38346
adeabdolan <adeabdolan@gmail.com>
2024-02-06 13:33:07 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Navbar from './components/Navbar';
+import Intro from './components/Intro';
+import About from './components/About';
+import Support from './components/Support';
+import Platforms from './components/Platforms';
+import Membership from './components/Membership';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Intro />
+      <About />
+      <Support />
+      <Platforms />
+      <Membership />
+      <Footer />
+       
+    </div>
+  );
+}
+
+export default App;
